3187f5ec53e1439f04152cf08b707a9b4b9db5d1
Nail Galimov <nailgalimov9@gmail.com>
2020-07-10 02:17:34 +0500

    fix after review

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-//import './index.css';
+import './index.css';
 // импорты классов
 import {Card} from './../scripts/components/Card.js';
 import {FormValidator} from './../scripts/components/FormValidator.js';
